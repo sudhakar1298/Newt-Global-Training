9b76275418a0d390ad7ab79950869ecf2d14a4e5
sudhakarpappu <sudhakarpappu123@gmail.com>
2026-05-04 22:35:05 +0530

    pro1

--- projects/ipl-Spark/rddapp.py
+++ projects/ipl-Spark/rddapp.py
@@ -2,19 +2,12 @@ from flask import Flask, render_template, request
 from pyspark.sql import SparkSession
 
 app = Flask(__name__)
-
-# Initialize Spark
 spark = SparkSession.builder.appName("IPL_RDD").master("local[*]").getOrCreate()
 
-# Load Data as DataFrames first (needed for schema)
 matches_df = spark.read.csv("archive/matches.csv", header=True, inferSchema=True)
 deliveries_df = spark.read.csv("archive/deliveries.csv", header=True, inferSchema=True)
-
-# Convert to RDD
 matches_rdd = matches_df.rdd
 deliveries_rdd = deliveries_df.rdd
-
-# Get column indexes
 match_cols = matches_df.columns
 delivery_cols = deliveries_df.columns
 
@@ -27,19 +20,18 @@ def index():
     page_size = 10
     offset_value = (page - 1) * page_size
 
-    # Convert RDD to dict
     matches_dict_rdd = matches_rdd.map(lambda row: row_to_dict(row, match_cols))
 
-    # Sort by date (descending)
+
     sorted_rdd = matches_dict_rdd.sortBy(lambda x: x['date'], ascending=False)
 
-    # Pagination
+    
     paged = sorted_rdd.zipWithIndex() \
                       .filter(lambda x: offset_value <= x[1] < offset_value + page_size) \
                       .map(lambda x: x[0]) \
                       .collect()
 
-    # Select required fields
+   
     rm_list = [
         {
             "id": m["id"],
@@ -57,14 +49,10 @@ def index():
 def match_details(match_id):
 
     matches_dict_rdd = matches_rdd.map(lambda row: row_to_dict(row, match_cols))
-
-    # Get match info
     info = matches_dict_rdd.filter(lambda x: int(x["id"]) == match_id).collect()[0]
 
-    # Convert deliveries
     deliveries_dict_rdd = deliveries_rdd.map(lambda row: row_to_dict(row, delivery_cols))
 
-    # Filter wickets
     wickets_rdd = deliveries_dict_rdd.filter(
         lambda x: int(x["match_id"]) == match_id and int(x["is_wicket"]) == 1
     )
