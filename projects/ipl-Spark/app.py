4e67164e6f8f44d7ce68ce18f3699738859750f2
sudhakarpappu <sudhakarpappu123@gmail.com>
2026-05-05 23:02:57 +0530

    pro

--- projects/ipl-Spark/app.py
+++ projects/ipl-Spark/app.py
@@ -1,27 +1,55 @@
 from flask import Flask, render_template, request
 from pyspark.sql import SparkSession
-from pyspark.sql.functions import col,desc
+from pyspark.sql.functions import col,desc,lower
 
 app = Flask(__name__)
 
-# Initialize Spark
+
 spark = SparkSession.builder.appName("IPL_Local").master("local[*]").getOrCreate()
 
-# Load Data
+
 matches_df = spark.read.csv("archive/matches.csv", header=True, inferSchema=True).sort(col("date").desc())
 deliveries_df = spark.read.csv("archive/deliveries.csv", header=True, inferSchema=True)
 
 @app.route('/')
-def index():  
+def index():
     
+    query = request.args.get('q', default='')
+    filter_type = request.args.get('filter_type', default='all') # New parameter
     page = request.args.get('page', default=1, type=int)
     page_size = 10
     offset_value = (page - 1) * page_size
-    paged_df = matches_df.offset(offset_value).limit(10)
-    rm_list = [x.asDict() for x in paged_df.select("id", "team1", "team2", "date").collect()]
-    
-    return render_template('index.html', matches=rm_list, page=page)
 
+    filtered_df = matches_df
+
+    
+    if query:
+        search_lower = query.lower()
+        
+        if filter_type == 'team':
+            
+            filtered_df = filtered_df.filter(
+                (lower(col("team1")).contains(search_lower)) | 
+                (lower(col("team2")).contains(search_lower))
+            )
+        elif filter_type == 'city':
+            
+            filtered_df = filtered_df.filter(lower(col("city")).contains(search_lower))
+        else:
+            
+            filtered_df = filtered_df.filter(
+                (lower(col("team1")).contains(search_lower)) |
+                (lower(col("team2")).contains(search_lower)) |
+                (lower(col("city")).contains(search_lower))
+            )
+    paged_df = filtered_df.offset(offset_value).limit(page_size)
+    rm_list = [x.asDict() for x in paged_df.select("id", "team1", "team2", "date", "city").collect()]
+    
+    return render_template('index.html', 
+                           matches=rm_list, 
+                           page=page, 
+                           query=query, 
+                           filter_type=filter_type)
 @app.route('/match/<int:match_id>')
 def match_details(match_id):    
     info = matches_df.filter(col("id") == match_id).toPandas().to_dict(orient='records')[0]
